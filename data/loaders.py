2992782bfd02c0b5cf8b93759d25671c5dd4105f
Benedict Quartey <benedictquartey@sps1bk9dxd3.ad.brown.edu>
2021-11-17 13:45:08 -0500

    change to loader size

--- data/loaders.py
+++ data/loaders.py
@@ -18,11 +18,11 @@ class _RolloutDataset(torch.utils.data.Dataset): # pylint: disable=too-few-publi
 
         if train:
             # self._files = self._files[:-600] #original
-            self._files = self._files[:-60]
+            self._files = self._files[:-9]
             print(f"Train Files Count: {len(self._files)}")
         else:
             # self._files = self._files[-600:] # original 
-            self._files = self._files[-60:]
+            self._files = self._files[-9:]
             print(f"Test Files Count: {len(self._files)}")
 
         
